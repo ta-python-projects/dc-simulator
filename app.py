7cb1bca355055871a21b97a28cac09e9afdc2cfc
ta-python-projects <taworkmail423@gmail.com>
2026-06-22 21:41:45 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import numpy as np
 import streamlit as st
 import io
 import pandas as pd
+import matplotlib.pyplot as plt
+
+plt.rcParams["font.family"] = "IPAexGothic"
 
 #-----------------------------------------
 # DCシミュレーション関数
